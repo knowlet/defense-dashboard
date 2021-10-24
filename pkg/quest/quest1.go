f2385554394bbe76268c1260b7f263d4954afacf
knowlet <knowlet3389@gmail.com>
2021-10-24 17:02:02 +0800

    feat: read quest data from csv

--- pkg/quest/quest1.go
+++ pkg/quest/quest1.go
@@ -4,36 +4,29 @@ import (
 	"context"
 	"defense-dashboard/model"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"gorm.io/gorm"
 )
 
-// quest1
-func Quest1(db *gorm.DB) {
-	type t struct {
-		id       uint
-		ip       string
-		hostname string
-		pass     bool
-	}
-
-	var teams = []t{
-		{1, "127.0.0.1", "example.com", false},
-	}
-
-	for _, team := range teams {
-		go func(team t) {
+// Subversion
+func Subversion(db *gorm.DB, data []map[string]interface{}) {
+	for idx, team := range data {
+		team["id"] = idx + 1 // team id begins from 1
+		go func(t map[string]interface{}) {
 			ctx := context.Background()
 			ctx, cancel := context.WithCancel(ctx)
-			req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("http://%s:2021", team.ip), nil)
-			req.Host = team.hostname
-			client := &http.Client{}
-			// Timeout: 5 * time.Second,
+			req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("http://%s/listing.php", t["ip"]), nil)
+			req.Host = t["hostname"].(string)
+			client := &http.Client{
+				// Timeout: 10 * time.Second,
+			}
 			go func() {
-				time.Sleep(time.Second * 5)
+				time.Sleep(time.Second * 10)
 				cancel()
 			}()
 			resp, err := client.Do(req)
@@ -41,14 +34,25 @@ func Quest1(db *gorm.DB) {
 				log.Println(err) // cancel caught
 				return
 			}
-			log.Println(resp.StatusCode)
-			// save to db
-			db.Create(&model.Event{
-				Log:     fmt.Sprintf("#%d: Service alive Team%d score +%d", 1, team.id, plus),
-				Point:   plus,
-				TeamID:  team.id,
-				QuestID: 1,
-			})
+			if resp.StatusCode == http.StatusOK {
+				// read body
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return
+				}
+
+				// check keywords
+				if strings.Contains(string(body), "WebSVN") {
+					// save to db
+					db.Create(&model.Event{
+						Log:     fmt.Sprintf("#%d: Service alive Team%d score +%d", 1, t["id"], plus),
+						Point:   plus,
+						TeamID:  uint(t["id"].(int)),
+						QuestID: 1,
+					})
+				}
+			}
 		}(team)
 	}
 }
--- pkg/score/score.go
+++ pkg/score/score.go
@@ -3,16 +3,20 @@ package score
 import (
 	"time"
 
+	"defense-dashboard/pkg/helper"
 	"defense-dashboard/pkg/quest"
 
 	"gorm.io/gorm"
 )
 
 func Scoring(db *gorm.DB, ticker *time.Ticker, quit chan bool) {
+	// read data
+	q1 := helper.ReadQ1("data/quest1.csv")
 	for {
 		select {
 		case <-ticker.C:
-			go quest.Quest1(db)
+			// 10/26
+			go quest.Subversion(db, q1)
 			go quest.Quest2(db)
 
 		case <-quit:
