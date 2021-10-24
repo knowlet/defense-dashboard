9eb00399fddd41e585bb3abd9f40d3dfc1703ba8
knowlet <knowlet3389@gmail.com>
2021-10-24 18:32:32 +0800

    feat: add quest5

---
+++ pkg/quest/quest5.go
@@ -0,0 +1,88 @@
+package quest
+
+import (
+	"bufio"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+var (
+	paths = []string{
+		"/default.aspx",
+		"/Login.aspx",
+		"/NewsDetail.aspx?ID=1",
+		"/Service.aspx",
+		"/DownloadFile.aspx",
+	}
+)
+
+// News
+func News(db *gorm.DB, data []map[string]interface{}) {
+	for idx, team := range data {
+		team["id"] = idx + 1 // team id begins from 1
+		go func(t map[string]interface{}) {
+			check := 0
+			for _, path := range paths {
+				log.Println("[+] Checking", path)
+				// generate random message
+				message := make([]byte, 32)
+				rand.Read(message)
+
+				// create request
+				data := url.Values{}
+				data.Set("MSG", string(message))
+
+				req, _ := http.NewRequest(
+					http.MethodPost,
+					fmt.Sprintf("http://%s%s", t["ip"], path),
+					strings.NewReader(data.Encode()))
+				req.Host = t["hostname"].(string)
+				client := &http.Client{
+					Timeout: 10 * time.Second,
+				}
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Println(err) // cancel caught
+					return
+				}
+				defer resp.Body.Close()
+				log.Println(resp.Request.URL.String())
+				log.Println("[+] Response", resp.Status)
+				if resp.StatusCode == http.StatusOK {
+					// read body
+					defer resp.Body.Close()
+					// 258d9e21979350a42abb02ad21f60bbf0f398766eea23c126f7ca15acdb33f08
+					scanner := bufio.NewScanner(resp.Body)
+					if err != nil {
+						return
+					}
+					// get first line
+					scanner.Scan()
+					messageMAC := []byte(scanner.Text())
+					log.Println("[+] Message MAC:", messageMAC)
+
+					// check hmac
+					mac := hmac.New(sha256.New, []byte("HITCON_DEFENSE_2021"))
+					mac.Write(message)
+					expectedMAC := mac.Sum(nil)
+					if hmac.Equal(messageMAC, expectedMAC) {
+						check++
+						log.Println("[+]", t["ip"], path, "check:", check)
+					}
+				}
+			}
+			if check == len(paths) {
+				plusPoint(db, t)
+			}
+		}(team)
+	}
+}
--- pkg/score/score.go
+++ pkg/score/score.go
@@ -12,12 +12,14 @@ import (
 func Scoring(db *gorm.DB, ticker *time.Ticker, quit chan bool) {
 	// read data
 	q1 := helper.ReadQ1("data/quest1.csv")
+	q5 := helper.ReadQ1("data/quest5.csv")
 	for {
 		select {
 		case <-ticker.C:
 			// 10/26
 			go quest.Subversion(db, q1)
 			go quest.Quest2(db)
+			go quest.News(db, q5)
 
 		case <-quit:
 			ticker.Stop()
