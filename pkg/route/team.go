1f5971c71aaaec7743df4604b71ab89aab2aa9eb
knowlet <knowlet3389@gmail.com>
2021-10-26 01:43:37 +0800

    feat: add back team id page

--- pkg/route/team.go
+++ pkg/route/team.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"defense-dashboard/model"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -14,6 +15,31 @@ type Controller struct {
 }
 
 func (h Controller) TeamHandler(c *gin.Context) {
+	id := c.Param("id")
+	team := model.Team{}
+	if err := h.DB.Preload("Events", func(db *gorm.DB) *gorm.DB {
+		return db.Order("created_at DESC")
+	}).First(&team, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Team not found"})
+		return
+	}
+
+	// calc score
+	score := 0
+	logs := ""
+	for _, e := range team.Events {
+		score += e.Point
+		logs += fmt.Sprintf("[%s] %s\n", e.CreatedAt.Format(time.Kitchen), e.Log)
+	}
+
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"team":  team.Name,
+		"score": score,
+		"logs":  logs,
+	})
+}
+
+func (h Controller) TeamsHandler(c *gin.Context) {
 	queryModel := []model.Team{}
 	if err := h.DB.Preload("Events", func(db *gorm.DB) *gorm.DB {
 		return db.Order("created_at DESC")
