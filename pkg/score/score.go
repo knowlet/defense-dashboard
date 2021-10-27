c62dc8d7d31eb5a372a3c1ace129194c00c4b0a0
knowlet <knowlet3389@gmail.com>
2021-10-27 23:33:32 +0800

    feat: enable 28 quest

--- pkg/score/score.go
+++ pkg/score/score.go
@@ -12,9 +12,9 @@ import (
 func Scoring(db *gorm.DB, ticker, ticker2 *time.Ticker, quit chan bool) {
 	// read data
 	// q1 := helper.ReadQ1("data/quest1.csv")
-	// q2 := helper.ReadQ1("data/quest2.csv")
+	q2 := helper.ReadQ1("data/quest2.csv")
 	// q3 := helper.ReadQ1("data/quest3.csv")
-	// q4 := helper.ReadQ1("data/quest4.csv")
+	q4 := helper.ReadQ1("data/quest4.csv")
 	// q5 := helper.ReadQ1("data/quest5.csv")
 	q6 := helper.ReadQ1("data/quest6.csv")
 	q7 := helper.ReadQ1("data/quest7.csv")
@@ -24,12 +24,15 @@ func Scoring(db *gorm.DB, ticker, ticker2 *time.Ticker, quit chan bool) {
 	for {
 		select {
 		case <-ticker2.C: // check only
+			// 10/26
 			// go quest.Subversion(db, q1, true)
-			// go quest.Exchange(db, q2, true)
+			go quest.Exchange(db, q2, true)
 			// go quest.OA(db, q3, true)
-			// go quest.Stack(db, q4, true)
+			go quest.Stack(db, q4, true)
 			// go quest.News(db, q5, true)
-			go quest.Blog(db, q6, false)
+
+			// 10/28
+			go quest.Chk(db, q6, false)
 			go quest.Blog(db, q7, false)
 			go quest.Git(db, q8, false)
 			go quest.Chat(db, q9, false)
@@ -37,13 +40,13 @@ func Scoring(db *gorm.DB, ticker, ticker2 *time.Ticker, quit chan bool) {
 		case <-ticker.C:
 			// 10/26
 			// go quest.Subversion(db, q1, false)
-			// go quest.Exchange(db, q2, false)
+			go quest.Exchange(db, q2, false)
 			// go quest.OA(db, q3, false)
-			// go quest.Stack(db, q4, false)
+			go quest.Stack(db, q4, false)
 			// go quest.News(db, q5, false)
 
 			// 10/28
-			go quest.Blog(db, q6, false)
+			go quest.Chk(db, q6, false)
 			go quest.Blog(db, q7, false)
 			go quest.Git(db, q8, false)
 			go quest.Chat(db, q9, false)
