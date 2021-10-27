1d33ba1c9e05306f9ffaa6a2f6711ee96071e341
knowlet <knowlet3389@gmail.com>
2021-10-28 02:02:37 +0800

    fix: exchange login issue

--- pkg/quest/quest2.go
+++ pkg/quest/quest2.go
@@ -16,10 +16,11 @@ func Exchange(db *gorm.DB, data []map[string]interface{}, ischeck bool) {
 		team["id"] = idx + 1 // team id begins from 1
 		go func(t map[string]interface{}) {
 			// check logon page
+			h := t["hostname"].(string)
 			resp1, err := request(
 				http.MethodGet,
 				fmt.Sprintf("https://%s/owa/auth/logon.aspx?replaceCurrent=1&url=", t["ip"]),
-				t["hostname"].(string), nil, nil)
+				h, nil, nil)
 			if err != nil {
 				log.Println("[-]", err) // cancel caught
 				healthcheck(db, quest2, t["id"].(int), ischeck, false)
@@ -27,16 +28,18 @@ func Exchange(db *gorm.DB, data []map[string]interface{}, ischeck bool) {
 			}
 			defer resp1.Body.Close()
 			if resp1.StatusCode != http.StatusOK {
+				log.Println("[-]", resp1.StatusCode)
 				healthcheck(db, quest2, t["id"].(int), ischeck, false)
 				return
 			}
 
+			domain := h[strings.Index(h, ".")+1:]
 			// querystring
 			data := url.Values{}
-			data.Set("destination", fmt.Sprintf("https://%s/owa", t["hostname"]))
+			data.Set("destination", fmt.Sprintf("https://%s/owa", h))
 			data.Set("flags", "4")
 			data.Set("forcedownlevel", "0")
-			data.Set("username", "JamesHarden@blueteam1.defense.hitcon")
+			data.Set("username", fmt.Sprintf("JamesHarden@%s", domain))
 			data.Set("password", "Def_2021.int")
 			data.Set("passwordText", "")
 			data.Set("isUtf8", "1")
@@ -47,7 +50,7 @@ func Exchange(db *gorm.DB, data []map[string]interface{}, ischeck bool) {
 			resp, err := request(
 				http.MethodPost,
 				fmt.Sprintf("https://%s/owa/auth.owa", t["ip"]),
-				t["hostname"].(string),
+				h,
 				strings.NewReader(data.Encode()), nil)
 			if err != nil {
 				log.Println("[-]", err) // cancel caught
@@ -64,9 +67,11 @@ func Exchange(db *gorm.DB, data []map[string]interface{}, ischeck bool) {
 					healthcheck(db, quest2, t["id"].(int), ischeck, false)
 					return
 				}
+				log.Println("[+]", url.String())
 				healthcheck(db, quest2, t["id"].(int), ischeck, url.Path == "/owa")
 				// healthcheck(db, quest2, t["id"].(int), ischeck, url.Path != "")
 			} else {
+				log.Println("[-]", resp.StatusCode)
 				healthcheck(db, quest2, t["id"].(int), ischeck, false)
 			}
 		}(team)
